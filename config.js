24c74becf4c040440c187b776062d191c48a5863
sibeijialat <48174109+sibeijialat@users.noreply.github.com>
2020-01-07 23:38:26 +0800

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  TOKEN: process.env.TOKEN,
-  READING_CHANNELS: (process.env.READING_CHANNELS || '').split(',').filter(Boolean),
-  WRITING_CHANNELS: (process.env.WRITING_CHANNELS || '').split(',').filter(Boolean),
-  WEBHOOKS: (process.env.WEBHOOKS || '').split(',').filter(Boolean),
-};
